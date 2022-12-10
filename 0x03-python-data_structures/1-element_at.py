743ded5a1a06108a977736b1a45eef91f53d02c6
Tabitht <imafidontabitha@gmail.com>
2022-12-10 08:35:18 +0000

    new files still on list and tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,9 @@ def element_at(my_list, idx):
     i = 0
     while i < len(my_list):
         if idx < 0:
-            return none
+            return None
         elif idx > len(my_list) or idx == len(my_list):
-            return none
+            return None
         else:
             return my_list[idx]
+        i = i + 1
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    k = len(my_list)
+    for i in range(k):
+        print("{:d}".format(my_list[k - 1]))
+        k = k - 1
