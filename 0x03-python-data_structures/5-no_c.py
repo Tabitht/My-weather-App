9566449a6ebc0ce7abc148d76a6d4396334851e8
Tabitht <imafidontabitha@gmail.com>
2022-12-10 09:15:17 +0000

    new files still on list and tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    for letters in my_string:
+        if letters in "Cc":
+            new = []
+            continue
+            new = my_string[:]
+        return new
